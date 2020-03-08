a506f1363fed0c4d427aa82ff006edaeeaf0b39a
Agnes Lam <agneslam12@gmail.com>
2020-03-08 16:14:57 +0800

    Updates again v2.0

--- microservices/booking/booking.php
+++ microservices/booking/booking.php
@@ -15,15 +15,26 @@
     <!-- Bootstrap libraries -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" type="text/css" href="css/bookstrap.css" />
+    <!-- Localhost JS-->
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.bundle.js" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.bundle.js.map" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.bundle.min" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.bundle.min.js.map" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.js" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.js.map" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.min.js" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.min.js.map" />
+
+    <!-- Localhost JQuery-->
+    <link rel="stylesheet" type="text/css" href="js/jquery-3.1.1.min.js" />
+    <link rel="stylesheet" type="text/css" href="js/jquery-2.1.1.js" />
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="" <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 </head>
