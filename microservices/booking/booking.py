f3e5cc1eb3dbb637cff4ce6a767f851bb74e200d
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-03-19 18:33:44 +0800

    Update booking.py
    jsonify for the by customer mobile to return all bookings belonging to their.
    
    Co-Authored-By: Agnes Lam <agnesxlam@users.noreply.github.com>

--- microservices/booking/booking.py
+++ microservices/booking/booking.py
@@ -2,13 +2,15 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-app = Flask (__name__)      # making book.py as a Flask app
+app = Flask(__name__)  # making book.py as a Flask app
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/g7t3_booking'
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/g7t3_booking'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 db = SQLAlchemy(app)
+
 
 class Booking(db.Model):
     __tablename__ = 'booking'
@@ -20,9 +22,10 @@ class Booking(db.Model):
     booking_date = db.Column(db.String(10), nullable=False)
     booking_price = db.Column(db.Float(precision=2), nullable=False)
 
-    def __init__(self, booking_id, customer_mobile, provider_mobile, booking_time, booking_date ,booking_price):
+    def __init__(self, booking_id, customer_mobile, provider_mobile,
+                 booking_time, booking_date, booking_price):
         self.booking_id = booking_id
-        self.customer_mobile = customer_mobile        
+        self.customer_mobile = customer_mobile
         self.provider_mobile = provider_mobile
         self.booking_time = booking_time
         self.booking_date = booking_date
@@ -30,36 +33,43 @@ class Booking(db.Model):
 
     def json(self):
         return {
-        "booking_id": self.booking_id, 
-        "customer_mobile": self.customer_mobile, 
-        "provider_mobile": self.provider_mobile,
-        "booking_time": self.booking_time,
-        "booking_date": self.booking_date,
-        "booking_price": self.booking_price
+            "booking_id": self.booking_id,
+            "customer_mobile": self.customer_mobile,
+            "provider_mobile": self.provider_mobile,
+            "booking_time": self.booking_time,
+            "booking_date": self.booking_date,
+            "booking_price": self.booking_price
         }
 
-@app.route("/booking")         # when will this page called? Default HTTP protocol is GET, even when not specified
+
+@app.route(
+    "/booking"
+)  # when will this page called? Default HTTP protocol is GET, even when not specified
 def getAll():
     #return "Get all Books"  # pull the data from the DB
     return jsonify({
         "bookings": [booking.json() for booking in Booking.query.all()]
-        }) #Book.query.all() is the same as " SELECT * FROM table_name "
+    })  #Book.query.all() is the same as " SELECT * FROM table_name "
+
 
 @app.route("/booking/<string:customer_mobile>", methods=['GET'])
 def findBooking(customer_mobile):
-    booking = Booking.query.filter_by(customer_mobile=customer_mobile).first() 
-    # .first() returns the FIRST RECORD : SELECT * FROM book WHERE isbn13 = <isbn13> LIMIT 1
-    if booking:
-        return jsonify(booking.json())
-    return jsonify({"message": "Booking not found"}), 404
+    allBookings = jsonify({
+        "bookings": [
+            booking.json() for booking in Booking.query.filter_by(
+                customer_mobile=customer_mobile)
+        ]
+    })
+    return allBookings
 
 
 @app.route("/booking/<string:booking_id>", methods=['POST'])
 def createBooking(booking_id):
     if (Booking.query.filter_by(booking_id=booking_id).first()):
         return jsonify({
-            "message": "A booking with Booking ID '{}' already exists.".format(booking_id)
-            }), 400
+            "message":
+            "A booking with Booking ID '{}' already exists.".format(booking_id)
+        }), 400
 
     data = request.get_json()
     print(data)
@@ -69,16 +79,20 @@ def createBooking(booking_id):
         db.session.add(booking)
         db.session.commit()
     except:
-        return jsonify({"message": "An error occurred creating the booking."}), 500
+        return jsonify({"message":
+                        "An error occurred creating the booking."}), 500
 
     return jsonify(booking.json()), 201
 
+
 @app.route("/booking/update/<string:booking_id>", methods=['POST'])
 def updateCustomer(booking_id):
-    if (not(Booking.query.filter_by(booking_id=booking_id).first())):
+    if (not (Booking.query.filter_by(booking_id=booking_id).first())):
         return jsonify({
-            "message": "A booking with Booking ID '{}' does not exists.".format(booking_id)
-            }), 400
+            "message":
+            "A booking with Booking ID '{}' does not exists.".format(
+                booking_id)
+        }), 400
 
     data = request.get_json()
     print(data)
@@ -94,10 +108,12 @@ def updateCustomer(booking_id):
         booking.booking_price = data['booking_price']
         db.session.commit()
     except:
-        return jsonify({"message": "An error occurred updating the booking."}), 500
+        return jsonify({"message":
+                        "An error occurred updating the booking."}), 500
 
     return jsonify(booking.json()), 201
-    
+
+
 if __name__ == "__main__":  # to run this application with out having the name app.py
     app.debug = True
-    app.run(host = '0.0.0.0',port=1002, debug=True)
+    app.run(host='0.0.0.0', port=1002, debug=True)
