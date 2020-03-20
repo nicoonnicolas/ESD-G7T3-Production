1717e8234abe2f9981075a0bf19c1068f9cfb1e2
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-03-20 17:24:05 +0800

    Update serviceprovider_trial.sql
    Changed the DB structure for service provider.
    
    Co-Authored-By: Agnes Lam <agnesxlam@users.noreply.github.com>
    Co-Authored-By: mingmiao-prog <mingmiao-prog@users.noreply.github.com>

--- sql scripts/serviceprovider_trial.sql
+++ sql scripts/serviceprovider_trial.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 20, 2020 at 06:09 AM
--- Server version: 8.0.18
--- PHP Version: 7.3.12
+-- Generation Time: Mar 20, 2020 at 08:37 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,21 +30,21 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `serviceprovider_trial`;
 CREATE TABLE IF NOT EXISTS `serviceprovider_trial` (
-  `sn` int(11) NOT NULL AUTO_INCREMENT,
-  `provider_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `provider_mobile` varchar(8) NOT NULL COMMENT 'Serial number for the service provider.',
+  `provider_name` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
   `provider_price` decimal(10,2) NOT NULL,
-  `provider_time` time NOT NULL,
-  `provider_day` date NOT NULL,
-  `provider_service` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`sn`)
+  `provider_time` varchar(8) NOT NULL,
+  `provider_day` varchar(3) NOT NULL,
+  `provider_service` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
+  PRIMARY KEY (`provider_mobile`,`provider_service`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `serviceprovider_trial`
 --
 
-INSERT INTO `serviceprovider_trial` (`sn`, `provider_name`, `provider_price`, `provider_time`, `provider_day`, `provider_service`) VALUES
-(1, 'Agnes Lam', '45.00', '09:00:00', '2020-03-20', 'Feeding');
+INSERT INTO `serviceprovider_trial` (`provider_mobile`, `provider_name`, `provider_price`, `provider_time`, `provider_day`, `provider_service`) VALUES
+('1', 'Agnes Lam', '45.00', '18:00:00', 'fri', 'Feeding');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
