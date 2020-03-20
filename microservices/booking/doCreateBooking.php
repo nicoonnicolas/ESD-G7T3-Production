b65f05952a172c5fbcc326857638a687bdfaf772
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-03-20 17:25:34 +0800

    Create doCreateBooking.php
    Retrieving the value from the form page of createBooking.php
    
    Co-Authored-By: Agnes Lam <agnesxlam@users.noreply.github.com>
    Co-Authored-By: mingmiao-prog <mingmiao-prog@users.noreply.github.com>

---
+++ microservices/booking/doCreateBooking.php
@@ -0,0 +1,15 @@
+<?php
+
+$customerMobile = $_POST['customer_mobile'];
+$providerMobile = $_POST['provider_mobile'];
+$providerService = $_POST['provider_service'];
+
+echo $customerMobile; echo $providerMobile; echo $providerService;
+
+// Add into using microservice booking.py
+// Change the booking id to AI.
+// Change provider service last row to customer mobile. 
+// Status (case close, can add review)
+// Provider Service needs to be added. 
+
+?>
