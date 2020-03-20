08d28a685cbb68af95132636c486f32be0f17e41
Mingmiao-prog <54536256+Mingmiao-prog@users.noreply.github.com>
2020-03-20 14:03:33 +0800

    create new serviceprovider_trial

---
+++ microservices/serviceprovider/serviceProvider_Trial.py
@@ -0,0 +1,105 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from os import environ
+
+
+app = Flask (__name__)      # making book.py as a Flask app
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/g7t3_serviceProvider'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+db = SQLAlchemy(app)
+
+class ServiceProvider(db.Model):
+    __tablename__ = 'serviceprovider_trial'
+    
+    provider_sn = db.Column(db.Integer, primary_key=True)
+    provider_name = db.Column(db.String(128), nullable = False)
+    provider_price = db.Column(db.Float(precision=2), nullable=False)
+    provider_time = db.Column(db.Time, nullable=True)
+    provider_day= db.Column(db.Date, nullable=True)
+    provider_service= db.Column(db.String(256), nullable=True)
+
+    def __init__(self, provider_sn, provider_name, provider_service, provider_time, provider_day, provider_price):
+        self.provider_sn = provider_sn
+        self.provider_name = provider_name
+        self.provider_service = provider_service        
+        self.provider_time = provider_time
+        self.provider_day = provider_day
+        self.provider_price = provider_price
+
+    def json(self):
+        return {
+        "provider_sn": self.provider_sn,
+        "provider_name": self.provider_name,
+        "provider_service": self.provider_service, 
+        "provider_time": self.provider_time,
+        "provider_day": self.provider_day,
+        "provider_price": self.provider_price
+        }
+
+@app.route("/serviceprovider_trial")         # when will this page called? Default HTTP protocol is GET, even when not specified
+def getAll():
+    #return "Get all Books"  # pull the data from the DB
+    return jsonify({
+        "serviceproviders": [serviceprovider.json() for serviceprovider in ServiceProvider.query.all()]
+        }) #Book.query.all() is the same as " SELECT * FROM table_name "
+
+# @app.route("/serviceprovider_trial/<string:provider_mobile>", methods=['GET'])
+# def findServiceProvider(provider_mobile):
+#     serviceprovider = ServiceProvider.query.filter_by(provider_mobile=provider_mobile).first() 
+#     # .first() returns the FIRST RECORD : SELECT * FROM book WHERE isbn13 = <isbn13> LIMIT 1
+#     if serviceprovider:
+#         return jsonify(serviceprovider.json())
+#     return jsonify({"message": "Service Provider not found"}), 404
+
+
+# @app.route("/serviceprovider/<string:provider_mobile>", methods=['POST'])
+# def createServiceProvider(provider_mobile):
+#     if (ServiceProvider.query.filter_by(provider_mobile=provider_mobile).first()):
+#         return jsonify({
+#             "message": "A service provider with Provider ID '{}' already exists.".format(provider_mobile)
+#             }), 400
+
+#     data = request.get_json()
+#     print(data)
+#     serviceprovider = ServiceProvider(provider_mobile, **data)
+
+#     try:
+#         db.session.add(serviceprovider)
+#         db.session.commit()
+#     except:
+#         return jsonify({"message": "An error occurred creating the service provider."}), 500
+
+#     return jsonify(serviceprovider.json()), 201
+
+# @app.route("/serviceprovider/update/<string:provider_mobile>", methods=['POST'])
+# def updateServiceProvider(provider_mobile):
+#     if (not(ServiceProvider.query.filter_by(provider_mobile=provider_mobile).first())):
+#         return jsonify({
+#             "message": "A service provider with Provider ID '{}' does not exists.".format(provider_mobile)
+#             }), 400
+
+#     data = request.get_json()
+#     print(data)
+#     serviceprovider = ServiceProvider(provider_mobile, **data)
+
+#     try:
+#         serviceprovider = ServiceProvider.query.filter_by(provider_mobile=provider_mobile).first()
+#         serviceprovider.provider_name = data['provider_name']
+#         serviceprovider.provider_service1 = data['provider_service1']
+#         serviceprovider.provider_service2 = data['provider_service2']
+#         serviceprovider.provider_service3 = data['provider_service3']
+#         serviceprovider.provider_price = data['provider_price']
+#         db.session.commit()
+#     except:
+#         return jsonify({"message": "An error occurred updating the service provider."}), 500
+
+#     return jsonify(serviceprovider.json()), 201
+    
+if __name__ == "__main__":  # to run this application with out having the name app.py
+    app.debug = True
+    app.run(host='0.0.0.0', port=1001, debug=True)
