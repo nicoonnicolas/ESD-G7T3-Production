517018715e6f1450b4b6ce107bef4353b6a9a7f9
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-03-22 17:22:01 +0800

    Update serviceProvider_Trial.php
    Copied the nav bar to standardize
    
    Co-Authored-By: Agnes Lam <agnesxlam@users.noreply.github.com>
    Co-Authored-By: mingmiao-prog <mingmiao-prog@users.noreply.github.com>

--- microservices/serviceprovider/serviceProvider_Trial.php
+++ microservices/serviceprovider/serviceProvider_Trial.php
@@ -29,25 +29,53 @@
         </script>
     </head>
     <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <img src="../../app/Paws-logo.png" width="140" height="60" alt="Paws">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="../../app/index.html">Home <span class="sr-only">(current)</span></a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Booking
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="../booking/createBooking.php">Create booking</a>
+                            <a class="dropdown-item" href="../booking/updateBooking.html">Update booking</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Service Providers
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="../serviceprovider/serviceProviderRegistration.php">Create Service Provider</a>
+                            <a class="dropdown-item" href="../serviceprovider/updateServiceProvider.php">Update Service Provider</a>
+                        </div>
+                    </li>
+                    <a class="nav-item nav-link" href="../review/createReview.html">Reviews</a>
+                </div>
+            </div>
+        </nav>
         <h1 class="display-4">Service Providers</h1>
         <div id="main_container" class="container">
-            <p>
-                <a class="btn btn-outline-info" href="../booking/booking.php">Bookings</a>
-                <a class='btn btn-outline-info' href='../customer/customer.php'>Customers</a> 
-                <a class='btn btn-outline-info' href='../serviceprovider/serviceprovider.php'>Service Providers</a>
-                <a class='btn btn-outline-info' href='../review/review.php'>Reviews</a>
-            </p>
             <table id="service_table" class='table table-striped' border='1'>
                 <thead class='thead-dark'>
                     <tr>
                         <th>Service Provider</th>
                         <th>Contact Number</th>
-                        <th colspan="3">Services Provided</th>
+                        <th>Services Provided</th>
+                        <th>Time</th>
+                        <th>Day</th>
                         <th>Price</th>
                     </tr>
                 <thead class='thead-dark'>
             </table>
-            <a id="addReviewBtn" class="btn btn-primary" href="add-serviceprovider.html">Add Provider</a>
+            <a id="addReviewBtn" class="btn btn-primary" href="../serviceprovider/serviceProviderRegistration.php">Add Provider</a>
         </div>
         <script>
             function showError(message) {
