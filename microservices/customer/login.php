e4fa8edbfeea1acd8ea4d8dea24be849795dcf44
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-03-22 17:26:43 +0800

    Update login.php
    Revamped the login page of customer. To be going out of commission soon.
    
    Co-Authored-By: Agnes Lam <agnesxlam@users.noreply.github.com>
    Co-Authored-By: mingmiao-prog <mingmiao-prog@users.noreply.github.com>

--- microservices/customer/login.php
+++ microservices/customer/login.php
@@ -1,18 +1,95 @@
 <!DOCTYPE HTML>
 <html>
-    <head></head>
+    <head>
+        <title>Customer Registration</title>
+        <meta charset="UTF-8">
+        <!-- Bootstrap libraries -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Latest compiled and minified CSS -->
+        <link 
+            rel="stylesheet" 
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+            integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" 
+            crossorigin="anonymous">
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+        <script 
+            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" 
+            integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+            crossorigin="anonymous">
+        </script>
+
+        <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+            integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+            crossorigin="anonymous">
+        </script>
+
+
+    </head>
     <body>
-        <form action="doLogin.php" method="POST">
-            <table>
-                <tr>
-                    <td>Mobile Number:</td>
-                    <td><input type="text" name="mobile_number"</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="submit" value="Login"/></td>
-                </tr>
-            </table>
-        </form>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <img src="../../app/Paws-logo.png" width="140" height="60" alt="Paws">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="../../app/index.html">Home <span class="sr-only">(current)</span></a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Booking
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="../booking/createBooking.php">Create booking</a>
+                            <a class="dropdown-item" href="../booking/updateBooking.html">Update booking</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Service Providers
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="../microservices/serviceprovider/createServiceProvider.html">Create Service Provider</a>
+                            <a class="dropdown-item" href="../microservices/serviceprovider/updateServiceProvider.html">Update Service Provider</a>
+                        </div>
+                    </li>
+                    <a class="nav-item nav-link" href="../microservices/review/createReview.html">Reviews</a>
+
+                </div>
+            </div>
+        </nav>
+
+        <br/>
+
+        <h1 class="display-4 text-center">Customer Login</h1>
+        <br/>
+        <div id="main-container" class="container" style = "width: 80%;">
+            <form action="doLogin.php" method="POST">
+                <div class = "row">
+                    <div class = "col-lg-6" style = "margin: 0 auto;">
+                        Mobile Number: <input type="text" name="mobile_number" class = "form-control">
+                    </div>
+                </div>
+                <br/>
+                <div class = "row">
+                    <div class = "col-lg-6" style = "margin: 0 auto;">
+                        <button type="submit" class = "btn btn-primary">Login</button>
+                    </div>
+                </div>
+                <br/>
+                <div class = "row">
+                    <div class = "col-lg-6 text-center" style = "margin: 0 auto;">
+                        <a href = "customerRegistration.html" style = "font-size: 12px;">Not an existing user? Click here to Sign Up</a>
+                    </div>
+                </div>
+            </form>
+        </div>
     </body>
 </html>
