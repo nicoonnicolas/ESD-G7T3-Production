5cd6fb7eb4224daf178c9e18f392af4fed98e953
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-03-24 18:53:32 +0800

    Update serviceProvider_Trial.py
    Inserted Functions for
    1. getProvider: to find all the existing services that the service provider provides.
    2. addService: to add services for existing and new service providers.
    3. getProvider: to find if service provider existed, this is for the service provider login page.

--- microservices/serviceprovider/serviceProvider_Trial.py
+++ microservices/serviceprovider/serviceProvider_Trial.py
@@ -46,7 +46,34 @@ def getAll():
     #return "Get all Books"  # pull the data from the DB
     return jsonify({
         "serviceProviders": [serviceprovider.json() for serviceprovider in ServiceProvider.query.all()]
-        }) 
+        })
+
+@app.route("/serviceprovider_trial/<string:provider_mobile>", methods=['GET'])
+def getAllServicesByProvider(provider_mobile):
+    return jsonify({
+        "serviceProviders": [serviceprovider.json() for serviceprovider in ServiceProvider.query.filter_by(
+                provider_mobile=provider_mobile)
+        ]
+    })
+
+@app.route("/serviceprovider_trial/login/<string:provider_mobile>", methods=['GET'])
+def getProvider(provider_mobile):
+    serviceprovider = ServiceProvider.query.filter_by(provider_mobile=provider_mobile).first()
+    if serviceprovider:
+        return jsonify(serviceprovider.json())
+    return jsonify({"message": "Service Provider not found"}), 404
+
+@app.route("/serviceprovider_trial/<string:provider_mobile>", methods=["POST"])
+def addService(provider_mobile):
+    data = request.get_json()
+    serviceProvider = ServiceProvider(provider_mobile, **data)
+    try:
+        db.session.add(serviceProvider)
+        db.session.commit()
+    except:
+        return jsonify({"message": "An error occurred creating the service provider."}), 500
+
+    return jsonify(serviceProvider.json()), 201
     
 if __name__ == "__main__":  # to run this application with out having the name app.py
     app.debug = True
