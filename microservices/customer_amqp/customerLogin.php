6fb1fd58dfbab3e8129d8ea30ead9c9c53af6bf6
Agnes Lam <agneslam12@gmail.com>
2020-03-27 23:17:56 +0800

    update customer login ui

--- microservices/customer_amqp/customerLogin.php
+++ microservices/customer_amqp/customerLogin.php
@@ -31,26 +31,71 @@
 
     </head>
     <body>
-        <h1 class="display-4 text-center">Customer Login</h1>
-        <br/>
-        <div id="main-container" class="container" style = "width: 80%;">
-            <div class = "row">
-                <div class = "col-lg-6" style = "margin: 0 auto;">
-                    Mobile Number: <input type="text" name="mobile_number" id="customer_mobile" class="form-control">
-                </div>
-            </div>
-            <br>
 
-            <button id="addCustomerBtn" type="button" class='btn btn-primary'>Login</button>
+    <style>
+        html, body {
+        height: 100%;
+        }
 
-            <br>
-            <div class = "row">
-                <div class = "col-lg-6 text-center" style = "margin: 0 auto;">
-                    <a href = "customerRegistration.html" style = "font-size: 12px;">Not an existing user? Click here to Sign Up</a>
-                </div>
-            </div>
-        </div>
-        <script>
+        body {
+        display: -ms-flexbox;
+        display: -webkit-box;
+        display: flex;
+        -ms-flex-align: center;
+        -ms-flex-pack: center;
+        -webkit-box-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        justify-content: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+        }
+
+        .form-signin {
+        width: 100%;
+        max-width: 330px;
+        padding: 15px;
+        margin: 0 auto;
+        }
+        .form-signin .checkbox {
+        font-weight: 400;
+        }
+        .form-signin .form-control {
+        position: relative;
+        box-sizing: border-box;
+        height: auto;
+        padding: 10px;
+        font-size: 16px;
+        }
+        .form-signin .form-control:focus {
+        z-index: 2;
+        }
+        .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+        }
+        .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        }
+    </style>
+
+        <form class="form-signin">
+        <!-- <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72"> -->
+        <h1 class="h3 mb-3 font-weight-normal">Paws System</h1>
+        <label for="inputEmail" class="sr-only">Mobile Number</label>
+        <input type="text" name="mobile_number" id="customer_mobile" class="form-control" placeholder="Mobile Number" required autofocus> <br>
+        <button class="btn btn-lg btn-primary btn-block" type="button" id="addCustomerBtn">Sign in</button>
+        <br>
+        <center><a href = "customerRegistration.html" style = "font-size: 12px;">Not an existing user? Click here to Sign Up</a></center>
+        <p class="mt-5 mb-3 text-muted">&copy; ESD 2020</p>
+        </form>
+    </body>
+
+    <script>
             $("#addCustomerBtn").click(function () {
                 $(async () => {
                     var customer_mobile = $('#customer_mobile').val();
@@ -76,5 +121,4 @@
                 });
             });
         </script>
-    </body>
 </html>
