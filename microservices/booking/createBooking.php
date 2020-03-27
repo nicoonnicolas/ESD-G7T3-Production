7e6123c2a6e77577252b35844ca49073d67cd40e
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-03-27 15:27:24 +0800

    Update createBooking.php
    Replaced header with standardized header.
    
    Co-Authored-By: Agnes Lam <agnesxlam@users.noreply.github.com>
    Co-Authored-By: mingmiao-prog <mingmiao-prog@users.noreply.github.com>

--- microservices/booking/createBooking.php
+++ microservices/booking/createBooking.php
@@ -36,38 +36,7 @@ if (isset($_SESSION['mobile_number'])) {
         </script>
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">
-                <img src="../../app/Paws-logo.png" width="140" height="60" alt="Paws">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="../../app/index.html">Home <span class="sr-only">(current)</span></a>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Booking
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="../booking/createBooking.php">Create booking</a>
-                            <a class="dropdown-item" href="../booking/updateBooking.html">Update booking</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Service Providers
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="../microservices/serviceprovider/createServiceProvider.html">Create Service Provider</a>
-                            <a class="dropdown-item" href="../microservices/serviceprovider/updateServiceProvider.html">Update Service Provider</a>
-                        </div>
-                    </li>
-                    <a class="nav-item nav-link" href="../microservices/review/createReview.html">Reviews</a>
-                </div>
-            </div>
-        </nav>
+        <?php include("../customer_amqp/header.php"); ?>
         <br>
         <h1 class="display-4" style = "padding-left: 10%;">Create Booking</h1>
         <div id="main-container" class="container">
@@ -98,7 +67,7 @@ if (isset($_SESSION['mobile_number'])) {
                 var serviceURL = "http://127.0.0.1:1001/serviceprovider_trial";
                 try {
                     const response = await fetch(serviceURL, {method: "GET"});
-                    console.log(response)
+                    console.log(response);
                     const data = await response.json();
                     console.log(data);
                     var serviceProviders = data.serviceProviders;
