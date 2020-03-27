a8375e3bd9ed2dd3d7a3db84dace1050af107000
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-03-27 14:21:21 +0800

    Update g7t3_allMicroservices.sql
    New database structure.

--- sql scripts/g7t3_allMicroservices.sql
+++ sql scripts/g7t3_allMicroservices.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 25, 2020 at 11:19 AM
+-- Generation Time: Mar 27, 2020 at 06:20 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.14
 
