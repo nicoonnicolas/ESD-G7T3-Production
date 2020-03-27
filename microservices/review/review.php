6e731888c63cd2945528be6ad4b8c13b8ccf3cbe
Agnes Lam <agneslam12@gmail.com>
2020-03-28 01:28:08 +0800

    update review

--- microservices/review/review.php
+++ microservices/review/review.php
@@ -5,86 +5,35 @@
         <meta charset="UTF-8">
         <!-- Bootstrap libraries -->
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <!-- Latest compiled and minified CSS -->
-        <link 
-            rel="stylesheet" 
-            href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-            integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" 
-            crossorigin="anonymous">
-
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-        <script 
-            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" 
-            integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
-            crossorigin="anonymous">
-        </script>
-
-        <script
-            src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
-            integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-            crossorigin="anonymous">
-        </script>
+        <?php include("../../app/globalCSS.php"); ?>
     </head>
+
+    <style>
+    .container-fluid {
+        padding: 30px;
+    }
+</style>
+
     <body>
+    <?php include("../../app/globalCustomerHeader.php"); ?>
+    <div class="container-fluid">
+        <div class="row">
+            <h1 class="display-4">Reviews</h1>          
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">
-                <img src="../../app/Paws-logo.png" width="140" height="60" alt="Paws">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="../../app/index.html">Home <span class="sr-only">(current)</span></a>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Booking
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="../microservices/booking/createBooking.html">Create booking</a>
-                            <a class="dropdown-item" href="../microservices/booking/updateBooking.html">Update booking</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Service Providers
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="../microservices/serviceprovider/createServiceProvider.html">Create Service Provider</a>
-                            <a class="dropdown-item" href="../microservices/serviceprovider/updateServiceProvider.html">Update Service Provider</a>
-                        </div>
-                    </li>
-                    <a class="nav-item nav-link" href="../microservices/review/createReview.html">Reviews</a>
-
-                </div>
-            </div>
-        </nav>
-
-        <h1 class="display-4" style = "padding-left: 10%;">Reviews</h1>
-        <div id="main_container" class="container">
-            <p>
-                <a class="btn btn-outline-info" href="../booking/booking.php">Bookings</a>
-                <a class='btn btn-outline-info' href='../customer/customer.php'>Customers</a> 
-                <a class='btn btn-outline-info' href='../serviceprovider/serviceprovider.php'>Service Providers</a>
-                <a class='btn btn-outline-info' href='../review/review.php'>Reviews</a>
-            </p>
-            <table id="review_table" class='table table-striped' border='1'>
-                <thead class='thead-dark'>
-                    <tr>
+            <table class="table" id="review_table">
+                <thead>
+                <tr>
                         <th>Booking ID</th>
                         <th>Stars</th>
                         <th>Comment</th>
                     </tr>
-                <thead class='thead-dark'>
+                </thead>
             </table>
-            <a id="addReviewBtn" class="btn btn-primary" href="addReview.html">Add a review</a>
         </div>
+        <a id="addReviewBtn" class="btn btn-primary" href="createReview.php">Add a review</a>
+    </div>
 
-        <footer class="page-footer font-small" style = "background-color: #007bff">
+        <!-- <footer class="page-footer font-small" style = "background-color: #007bff">
 
             <!-- Copyright -->
             <div class="footer-copyright text-center py-3" style = "color: white;">© 2020 Copyright:
@@ -92,38 +41,38 @@
             </div>
             <!-- Copyright -->
 
-        </footer>
-
-        <script>
-
-            function showError(message) {
-                $("#review_table").hide();
-                $("#main_container").append("<label>" + message + "</label>");
-            }
-            $(async() => {
-                var serviceURL = "http://127.0.0.1:1003/review";
-                try {
-                    const response = await fetch(serviceURL, {method: "GET"});
-                    const data = await response.json();
-                    var reviews = data.reviews;
-
-                    if (!reviews || !reviews.length) {
-                        showError("No Reviews Found!");
-                    } else {
-                        var rows = "";
-                        for (const review of reviews) {
-                            var eachRow =
-                                    "<td>" + review.booking_id + "</td>" +
-                                    "<td>" + review.review_star + "</td>" +
-                                    "<td>" + review.review_comment + "</td>";
-                            rows += "<tbody><tr>" + eachRow + "</tr></tbody>";
-                        }
-                        $("#review_table").append(rows);
-                    }
-                } catch (error) {
-                    showError("There is a problem retrieving books data, please try again later.<br>" + error);
-                }
-            });
-        </script>
+        <!-- </footer> -->
     </body>
+
+    <script>
+
+function showError(message) {
+    $("#review_table").hide();
+    $("#main_container").append("<label>" + message + "</label>");
+}
+$(async() => {
+    var serviceURL = "http://127.0.0.1:1003/review";
+    try {
+        const response = await fetch(serviceURL, {method: "GET"});
+        const data = await response.json();
+        var reviews = data.reviews;
+
+        if (!reviews || !reviews.length) {
+            showError("No Reviews Found!");
+        } else {
+            var rows = "";
+            for (const review of reviews) {
+                var eachRow =
+                        "<td>" + review.booking_id + "</td>" +
+                        "<td>" + review.review_star + "</td>" +
+                        "<td>" + review.review_comment + "</td>";
+                rows += "<tbody><tr>" + eachRow + "</tr></tbody>";
+            }
+            $("#review_table").append(rows);
+        }
+    } catch (error) {
+        showError("There is a problem retrieving books data, please try again later.<br>" + error);
+    }
+});
+</script>
 </html>
