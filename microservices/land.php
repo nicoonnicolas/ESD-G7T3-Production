c7b44d4dc3e7d48ed4fcc3c6dc0ab370c2b7ae2d
Mingmiao-prog <54536256+Mingmiao-prog@users.noreply.github.com>
2020-03-28 14:20:35 +0800

    improve land ui
    Co-Authored-By: Agnes Lam <agnesxlam@users.noreply.github.com>

--- microservices/land.php
+++ microservices/land.php
@@ -1,79 +1,90 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta name="viewport" content="width=device-width">
-
-        <title>Landing</title>
-
-        <link rel="stylesheet" href="">
-        <!--[if lt IE 9]>
-          <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
+        <title>Landing Page</title>
+        <meta charset="UTF-8">
         <!-- Bootstrap libraries -->
-        <meta name="viewport" 
-              content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <?php include("../app/globalCSS.php"); ?>
 
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet"
-              href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-              integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" 
-              crossorigin="anonymous">
-
-        <!-- Latest compiled and minified JavaScript -->
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script 
-        src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-        <script 
-            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
-            integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
-        crossorigin="anonymous"></script>
-
-        <script 
-            src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
-            integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-        crossorigin="anonymous"></script>
     </head>
     <body>
-        <div class="jumbotron jumbotron-fluid">
-            <h1 style = "padding-left: 4%; padding-bottom: 3%; text-align: center">
-                Welcome Back to PAWs!
-            </h1>
-            <h2 style = "padding-left: 4%; padding-bottom: 3%; text-align: center">
-                Today I am a...
-            </h2>
-            <div class="row">
-                <div class="col-sm-6" style = "padding-left: 4%;">
-                    <div class="card">
+
+    <style>
+        html, body {
+        height: 100%;
+        }
+
+        body {
+        display: -ms-flexbox;
+        display: -webkit-box;
+        display: flex;
+        -ms-flex-align: center;
+        -ms-flex-pack: center;
+        -webkit-box-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        justify-content: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+        }
+
+        .form-signin {
+        width: 100%;
+        max-width: 330px;
+        padding: 15px;
+        margin: 0 auto;
+        }
+        .form-signin .checkbox {
+        font-weight: 400;
+        }
+        .form-signin .form-control {
+        position: relative;
+        box-sizing: border-box;
+        height: auto;
+        padding: 10px;
+        font-size: 16px;
+        }
+        .form-signin .form-control:focus {
+        z-index: 2;
+        }
+        .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+        }
+        .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        }
+    </style>
+    <body>
+    <div class="container">
+  <div class="row">
+    <div class="col"><h1>Welcome to Paws System<br></h1><p>Login to your respective account.</p></div>
+  </div>
+  <div class="row">
+    <div class="col-6">                    
+    <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Customer</h5>
                             <p class="card-text">Click below to login as Customer</p>
                             <a href="customer_amqp/customerLogin.php" class="btn btn-primary">Customer Login</a>
                         </div>
-                    </div>
-                </div>
-                <div class="col-sm-6" style = "padding-left: 4%;">
-                    <div class="card">
+                    </div></div> 
+    <div class="col-6">                  <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Service Provider</h5>
                             <p class="card-text">Click below to login as Service Provider</p>
                             <a href="serviceprovider/serviceProviderLogin.php" class="btn btn-primary">Provider Login</a>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <footer class="page-footer font-large" style="background-color: #007bff">
-            <div class="footer-copyright text-center py-3" style="color: white;">© 2020 Copyright: ESD G7T3 (SMU AY2019/20 Term 2)
-                <marquee SCROLLAMOUNT=14 onmouseover="this.stop();" onmouseout="this.start();" style="font-size: 18px; font-weight: bold">
-                    Hello visitor! Please select your usage for today. If there are any problems with the webpage, please create a ticket or inform 
-                    the webmaster of the error. Where possible, include the error message that was reflected on the webpage. Have a pleasant day!
-                </marquee>
-            </div>
-        </footer>
+                    </div></div>
+  </div>
+
+</div>
+
 
     </body>
 </html>
