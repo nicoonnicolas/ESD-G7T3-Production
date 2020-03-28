39c2983d413fc99e3db3b97ae474b8cb304cb39f
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-03-28 16:12:59 +0800

    Update StripePayment.php
    Moved StripePayment.php into payment MS.

--- microservices/payment/StripePayment.php
+++ microservices/payment/StripePayment.php
@@ -10,7 +10,7 @@
 
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="../css/style.css">
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
@@ -62,7 +62,7 @@
 
   <script src="https://js.stripe.com/v3/"></script>
 
-  <script src="./js/charge.js"></script>
+  <script src="../js/charge.js"></script>
 
 </body>
 
