c7eef7bfc99d164afcc8a162136753f3245e9de8
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-03-28 14:40:11 +0800

    Update createReview.php
    Retrieving the data from booking.php page and function to ensure that the data is first checked in the db first.

--- microservices/review/createReview.php
+++ microservices/review/createReview.php
@@ -1,5 +1,30 @@
 <?php
+session_start();
 header("Access-Control-Allow-Origin: *");
+if (isset($_GET['booking_id'])) {
+    $bookingID = $_GET['booking_id'];
+}
+$HOST = "localhost";
+$USERNAME = "root";
+$PASSWORD = "";
+$DB = "g7t3_booking";
+$link = mysqli_connect($HOST, $USERNAME, $PASSWORD, $DB, "3306");
+if (!$link) {
+    die(mysqli_error($link));
+}
+
+if (isset($_GET['booking_id'])) {
+    $bookingID = $_GET['booking_id'];
+    $query = "SELECT * FROM booking WHERE booking_id = $bookingID";
+    $result = mysqli_query($link, $query);
+    if ($result) {
+        echo "<script> alert('Review has already been inserted! No more actions required.') </script>";
+        echo "<script> window.location.replace('../booking/booking.php'); </script>";
+    }
+} else {
+    echo "<script> alert('Booking Status Change: UNSUCCESSFUL :( ') </script>";
+    $message = '<a href="serviceProviderBooking_Trial.php">Return to previous page</a></h4>';
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,42 +39,44 @@ header("Access-Control-Allow-Origin: *");
     </head>
 
     <style>
-    .container-fluid {
-        padding: 30px;
-    }
-</style>
+        .container-fluid {
+            padding: 30px;
+        }
+    </style>
     <body>
-<?php include("../../app/globalCustomerHeader.php"); ?>
-<div class="container-fluid">
-        <div class="row">
-            <h1 class="display-4">Create Review</h1>    
-        </div>
+        <?php include("../../app/globalCustomerHeader.php"); ?>
+        <div class="container-fluid">
+            <div class="row">
+                <h1 class="display-4">Create Review</h1>    
+            </div>
 
-        <form id='add_review_form' method="post">
-        <div class="form-group">
-    <label for="booking_id">Booking ID</label>
-    <input type="booking_id" class="form-control" id="booking_id" placeholder="Booking ID">
-  </div>
-          <div class="form-group">
-            <label for="review_star">Review Stars</label>
-            <select class="form-control" id="review_star">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </div>
-        <div class="form-group">
-          <label for="review_comment">Review Comments</label>
-          <textarea class="form-control" id="review_comment" rows="3"></textarea>
-        </div>
-      </form>
-      <button id="addReviewBtn" type="submit" class='btn btn-primary'>Create Review</button>
+            <form id='add_review_form' method="post">
+                <div class="form-group">
+                    <label for="booking_id">Booking ID</label>
+                    <input type="booking_id" class="form-control" id="booking_id" placeholder="Booking ID">
+                </div>
+                <div class="form-group">
+                    <label for="review_star">Review Stars</label>
+                    <select class="form-control" id="review_star">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="review_comment">Review Comments</label>
+                    <textarea class="form-control" id="review_comment" rows="3"></textarea>
+                </div>
+            </form>
+            <button id="addReviewBtn" type="submit" class='btn btn-primary'>Create Review</button>
     </body>
 
     <script>
+        $('#booking_id').val("<?php echo $bookingID ?>");
         $('#review_table').hide();
+
         function showError(message) {
             // Hide the table and button in the event of error
             $('#review_table').hide();
@@ -57,33 +84,29 @@ header("Access-Control-Allow-Origin: *");
 
             // Display an error under the main container
             $('#main-container')
-                .append("<label class='errormsg'>" + message + "</label>");
+                    .append("<label class='errormsg'>" + message + "</label>");
         }
 
         check = [];
-
         //$("#submit").click(function () {
         $("#addReviewBtn").click(function () {
             $(async () => {
                 var booking_id = $('#booking_id').val();
-                console.log(booking_id)
+                console.log(booking_id);
                 var review_star = $('#review_star').val();
                 var review_comment = $('#review_comment').val();
                 var serviceURL = "http://localhost:1003/review" + "/" + booking_id;
-                console.log(serviceURL)
-
+                console.log(serviceURL);
                 try {
                     const response =
-                        await fetch(
-                            serviceURL, { 
-                                method: 'POST', 
-                                headers: { "Content-Type": "application/json" }, 
-                                body: JSON.stringify({ review_star: review_star, review_comment: review_comment })  
-                             });
-
-                    console.log(response)
+                            await fetch(
+                                    serviceURL, {
+                                        method: 'POST',
+                                        headers: {"Content-Type": "application/json"},
+                                        body: JSON.stringify({review_star: review_star, review_comment: review_comment})
+                                    });
+                    console.log(response);
                     const data = await response.json();
-                    
                     //var books = data; //the arr is in v.books of the JSON data
                     // array or array.length are falsy
                     //console.log(books);
@@ -93,18 +116,16 @@ header("Access-Control-Allow-Origin: *");
                     //    $('#booksTable').hide();
                     //    showError('Books list empty or undefined.');
                     //}
-                
-                    
+
+
                 } catch (error) {
                     $('.errormsg').remove();
                     // Errors when calling the service; such as network error, 
                     // service offline, etc
                     showError('There is a problem retrieving books data, please try again later.<br />' + error);
-
                 } // error
-            
-            });
 
+            });
         })
     </script>
 </html>
