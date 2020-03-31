93afb20b8bb6f21599e510d12abd4beb369d7d8d
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-03-31 17:33:00 +0800

    Update charge.php
    Change port num to 3306. Payment button works to update using Microservice and Return to booking page hyperlink is added.
    
    Co-Authored-By: kongyujie <kongyujie@users.noreply.github.com>

--- microservices/payment/charge.php
+++ microservices/payment/charge.php
@@ -1,12 +1,15 @@
-<?php 
+<?php
+session_start();
 $HOST = "localhost";
 $USERNAME = "root";
 $PASSWORD = "";
 $DB = "g7t3_booking";
-$link = mysqli_connect($HOST, $USERNAME, $PASSWORD, $DB, "3308");
-?>
+$link = mysqli_connect($HOST, $USERNAME, $PASSWORD, $DB, "3306");
 
-<?php
+if (isset($_SESSION['mobile_number'])) {
+    $customerMobile = $_SESSION['mobile_number'];
+}
+
 require_once('vendor/autoload.php');
 \Stripe\Stripe::setApiKey('sk_test_yPcdYCNGIPIsUlTNnWmpnr0400YTRYu5gF');
 
@@ -14,15 +17,10 @@ $bookingPrice = $_POST['booking_price'];
 $paymentID = $_POST['payment_id'];
 
 // Sanitize POST Array
-
 $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
-
 $first_name = $POST['first_name'];
-
 $last_name = $POST['last_name'];
-
 $email = $POST['email'];
-
 $token = $POST['stripeToken'];
 
 // Creating a customer in Stripe
@@ -32,28 +30,19 @@ $customer = \Stripe\Customer::create(array(
         ));
 
 // Charge Customer
-
 $charge = \Stripe\Charge::create(array(
-            "amount" => (float)$bookingPrice * 100,
+            "amount" => (float) $bookingPrice * 100,
             "currency" => "sgd",
             "customer" => $customer->id
         ));
 
 print_r($charge);
 
-// if (!isset($charge)) {
-//     echo "<script>alert('Unsuccesful payment')</script>";
-//     header("Location: ../booking/booking.php");
-// }else{
-//     echo "Unsucessful payment";
-//     header("Location: ../booking/booking.php");
-// }
+if (isset($_SESSION['mobile_number'])) {
+    $customerMobile = $_SESSION['mobile_number'];
+}
 ?>
-
-
-
 <html> 
-
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -74,9 +63,7 @@ print_r($charge);
         <!-- Latest compiled and minified JavaScript -->
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
     </head>
@@ -85,32 +72,30 @@ print_r($charge);
 
         <div id = "main-container"> </div>
 
-        <button id = 'updatePaymentStatusButton' class = 'btn btn-primary' href = '../booking/booking.php'> Update Payment Status</button>
+        <button id = 'updatePaymentStatusButton' class = 'btn btn-primary' href = '../booking/booking.php'> 
+            Update Payment Status
+        </button>
+        <br>
+        <a hrefclass = 'btn btn-primary' href = '../booking/booking.php?mobile_number=<?php echo $customerMobile ?>'> 
+            >>Return to Booking Page<<
+        </a>
 
         <script>
             var message = "There is an error";
-
             function showError(message) {
                 // Hide the table and button in the event of error
                 $('#updateServiceProviderBtn').hide();
             }
-
             check = [];
-
             //$('#submit').click(function () {
             $('#updatePaymentStatusButton').click(function () {
-
                 $(async() => {
-
 <?php
 $bookingID = $_POST['booking_id'];
 ?>
-
                     var booking_id = <?php echo $bookingID ?>;
-
                     var serviceURL = 'http://127.0.0.1:1002/booking/payment' + '/' + booking_id;
                     console.log(serviceURL)
-
                     try {
                         const response =
                                 await fetch(
@@ -123,21 +108,15 @@ $bookingID = $_POST['booking_id'];
                                                 "booking_payment_status": 1
                                             })
                                         });
-
-                        console.log(response)
+                        console.log(response);
                         const data = await response.json();
-
+                        alert("Payment Successful! You may close this page or return to booking below!");
                     } catch (error) {
                         $('.errormsg').remove();
-                        // Errors when calling the service; such as network error, 
-                        // service offline, etc
                         alert("Failed");
-
-                    } // error
-
+                    }
                 });
-
-            })
+            });
         </script>
     </body>
 
