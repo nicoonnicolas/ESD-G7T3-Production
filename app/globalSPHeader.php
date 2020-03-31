59a101155598c739037b59cf0f31c18e55c9c402
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-03-31 17:30:34 +0800

    Update globalSPHeader.php
    Changed the url for the header page.

--- app/globalSPHeader.php
+++ app/globalSPHeader.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="../serviceprovider/serviceProviderIndex.php">Paws System</a>
+    <a class="navbar-brand" href="../microservices/serviceprovider/serviceProviderIndex.php">Paws System</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
             aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -17,7 +17,7 @@
                     <a class="nav-link" href="../review/review.php">All Reviews</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../booking/booking.php">All Bookings</a>
+                    <a class="nav-link" href="../serviceprovider/serviceProviderBooking_Trial.php">All Bookings</a>
                 </li>
                 <a class="nav-item nav-link" href="../doLogout.php">Logout</a>
             </ul>
