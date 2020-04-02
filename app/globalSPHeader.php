720d1d41b53f182351ec1e0729dedcbe4d9e325c
Nicolas WIJAYA <nicolasw.2018@smu.edu.sg>
2020-04-03 01:39:34 +0800

    Update globalSPHeader.php
    Booking Acknowledgement Hyperlink included.

--- app/globalSPHeader.php
+++ app/globalSPHeader.php
@@ -1,25 +1,28 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="../microservices/serviceprovider/serviceProviderIndex.php">Paws System</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
             aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item">
-                    <a class="nav-link" href="../customer_amqp/customer_amqp.php">All Customers</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../serviceprovider/serviceProvider_Trial.php">All Service Providers</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../review/review.php">All Reviews</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../serviceprovider/serviceProviderBooking_Trial.php">All Bookings</a>
-                </li>
-                <a class="nav-item nav-link" href="../doLogout.php">Logout</a>
-            </ul>
-        </div>
-    </nav>
+                <a class="nav-link" href="../customer_amqp/customer_amqp.php">All Customers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../serviceprovider/serviceProvider_Trial.php">All Service Providers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../review/review.php">All Reviews</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../serviceprovider/serviceProviderBooking_Trial.php">All Bookings</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../payment/acknowledgePayment.php">My Payments</a>
+            </li>
+            <a class="nav-item nav-link" href="../doLogout.php">Logout</a>
+        </ul>
+    </div>
+</nav>
